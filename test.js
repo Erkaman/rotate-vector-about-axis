26011c2d00a7799eac18ce4340d32d691ffdaa43
Eric Arnebäck <arnebackeric@gmail.com>
2016-04-11 18:09:37 +0200

    made the tests stronger.

--- test.js
+++ test.js
@@ -3,22 +3,61 @@ var test    = require('tape')
 var almostEqual    = require('almost-equal')
 var vec3 = require('gl-vec3')
 
-
-test('rotateVectorAboutAxis', function(t) {
+function testVec3Equal(t, expected, actual) {
 
   var eps = 0.00001;
 
-  var rotated = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], Math.PI * 0.5  )
-  var expected = vec3.fromValues(1,0,0)
-  t.ok(almostEqual(rotated[0], expected[0], eps ) )
-  t.ok(almostEqual(rotated[1], expected[1], eps ) )
-  t.ok(almostEqual(rotated[2], expected[2], eps ) )
+  var testName = "Test " + "[" + actual + "]" + " = " + "[" + expected + "]"
 
-  rotated = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], -Math.PI * 0.5  )
+  t.ok(almostEqual(actual[0], expected[0], eps ), testName + ", x")
+  t.ok(almostEqual(actual[1], expected[1], eps ), testName + ", y")
+  t.ok(almostEqual(actual[2], expected[2], eps ), testName + ", z")
+}
+
+test('rotateVectorAboutAxis1', function(t) {
+
+  var eps = 0.00001;
+
+  var actual = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], Math.PI * 0.5  )
+  var expected = vec3.fromValues(1.0,0,0)
+  testVec3Equal(t, expected, actual)
+
+  actual = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], Math.PI * 1.0  )
+  expected = vec3.fromValues(0,0,-1)
+  testVec3Equal(t, expected, actual)
+
+  actual = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], -Math.PI * 0.5  )
   expected = vec3.fromValues(-1,0,0)
-  t.ok(almostEqual(rotated[0], expected[0], eps ) )
-  t.ok(almostEqual(rotated[1], expected[1], eps ) )
-  t.ok(almostEqual(rotated[2], expected[2], eps ) )
+  testVec3Equal(t, expected, actual)
+
+  actual = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], Math.PI * 1.5  )
+  expected = vec3.fromValues(-1,0,0)
+  testVec3Equal(t, expected, actual)
+
+  actual = rotateVectorAboutAxis( [0,0,1], [0, 1,0 ], Math.PI * 2.0  )
+  expected = vec3.fromValues(0,0,1)
+  testVec3Equal(t, expected, actual)
   
+  t.end()
+})
+
+test('rotateVectorAboutAxis2', function(t) {
+
+  var eps = 0.00001;
+
+  var sqrt2rcp = 1.0 / Math.sqrt(2);
+
+  var actual = rotateVectorAboutAxis( [sqrt2rcp,sqrt2rcp,0], [-sqrt2rcp,sqrt2rcp,0 ], Math.PI * 0.5  )
+  var expected = vec3.fromValues(0,0,-1)
+  testVec3Equal(t, expected, actual)
+
+  actual = rotateVectorAboutAxis( [sqrt2rcp,sqrt2rcp,0], [-sqrt2rcp,sqrt2rcp,0 ], Math.PI * 1.0  )
+  expected = vec3.fromValues(-sqrt2rcp,-sqrt2rcp,0)
+  testVec3Equal(t, expected, actual)
+
+  actual = rotateVectorAboutAxis( [sqrt2rcp,sqrt2rcp,0], [-sqrt2rcp,sqrt2rcp,0 ], Math.PI * 1.5  )
+  expected = vec3.fromValues(0,0,1)
+  testVec3Equal(t, expected, actual)
+
   t.end()
 })
